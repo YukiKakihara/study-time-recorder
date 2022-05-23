4a7a9b86debb351ef02a24d577b0c5583ea20e28
yuki.kakihara <argentina.kyg@gmail.com>
2022-05-23 09:28:33 +0900

    API_KEYを環境変数に設定した

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+    NOTION_KEY: process.env.NOTION_KEY
+  },
   reactStrictMode: true,
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
 
 const Page: NextPage = () => {
-  return <>hoge</>;
+  return (
+    <>
+      <div>NOTION_KEY：{process.env.NOTION_KEY}</div>
+      <div>NOTION_DATABASE_ID：{process.env.NOTION_DATABASE_ID}</div>
+    </>
+  );
 };
 
 export default Page;
