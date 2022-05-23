04b02a5b2a6439713bd8c5a89250ef9cdccd44ec
yuki.kakihara <argentina.kyg@gmail.com>
2022-05-23 11:53:43 +0900

    APIの型をちゃんと書いた

--- pages/api/add_memo.ts
+++ pages/api/add_memo.ts
@@ -1,7 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Client } from '@notionhq/client';
 import { CreatePageResponse } from '@notionhq/client/build/src/api-endpoints';
-import { formatDate } from 'utils/formatDate';
+
+export type RequestBody = {
+  date: string;
+  title: string;
+};
 
 type ResponseBody = {
   body: CreatePageResponse;
@@ -11,32 +15,36 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseBody>
 ) {
-  const notion = new Client({ auth: process.env.NOTION_KEY });
-  const databaseId = process.env.NOTION_DATABASE_ID || '';
-  const datePropertyKey = '%3B%3E_~';
+  if (req.method !== 'POST') {
+    res.status(404).end();
+  }
 
-  const add = async (date: string, text: string) =>
-    await notion.pages.create({
-      parent: { database_id: databaseId, type: 'database_id' },
-      properties: {
-        [datePropertyKey]: {
-          date: { end: null, start: date },
-          type: 'date',
-        },
-        title: {
-          title: [
-            {
-              text: {
-                content: text,
-              },
-            },
-          ],
-          type: 'title',
-        },
+  const reqBody: RequestBody = JSON.parse(req.body);
+  const { date, title } = reqBody;
+
+  const notion: Client = new Client({ auth: process.env.NOTION_KEY });
+  const databaseId: string = process.env.NOTION_DATABASE_ID || '';
+  const datePropertyKey: string = '%3B%3E_~';
+
+  const response: CreatePageResponse = await notion.pages.create({
+    parent: { database_id: databaseId, type: 'database_id' },
+    properties: {
+      [datePropertyKey]: {
+        date: { end: null, start: date },
+        type: 'date',
       },
-    });
-
-  const response = await add(formatDate(new Date()), 'テスト');
+      title: {
+        title: [
+          {
+            text: {
+              content: title,
+            },
+          },
+        ],
+        type: 'title',
+      },
+    },
+  });
 
   res.status(200).json({ body: response });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import type { NextPage } from 'next';
+import { formatDate } from 'utils/formatDate';
 
 const Page: NextPage = () => {
   return (
     <>
       <div>NOTION_KEY：{process.env.NOTION_KEY}</div>
       <div>NOTION_DATABASE_ID：{process.env.NOTION_DATABASE_ID}</div>
+      <button onClick={onClick}>click</button>
     </>
   );
+};
+
+const onClick = () => {
+  fetch('/api/add_memo', {
+    body: JSON.stringify({
+      date: formatDate(new Date()),
+      title: 'onClickテスト',
+    }),
+    method: 'POST',
+  });
 };
 
 export default Page;
