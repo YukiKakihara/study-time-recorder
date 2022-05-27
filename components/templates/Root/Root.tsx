6624f83fcad0e42d69372766cc3f6dae9b83bffb
yuki.kakihara <argentina.kyg@gmail.com>
2022-05-28 08:56:05 +0900

    分秒の部分にデザインを当てた

--- components/templates/Root/Root.tsx
+++ components/templates/Root/Root.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Box, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Box, BoxProps, FormControl, FormLabel, Input } from '@chakra-ui/react';
 
-type Props = {
-  className?: string;
-};
-
-export const Component: React.FC<Props> = ({ className }) => {
+const Component: React.FC<BoxProps> = (props) => {
   return (
-    <Box>
+    <Box {...props}>
+      <Box fontSize={40} mb={16} textAlign="center">
+        StudyTimeRecorder
+      </Box>
       <FormControl>
-        <div>StudyTimeRecorder</div>
-        <div>○分勉強したことを記録しました</div>
-        <FormLabel htmlFor="minite">分</FormLabel>
-        <Input id="minite" type="minite" />
-        <FormLabel htmlFor="second">秒</FormLabel>
-        <Input id="second" type="second" />
+        {/* <div>○分勉強したことを記録しました</div> */}
+        <Box display="flex" justifyContent="center">
+          <Input id="minite" sx={inputStyle} type="minite" />
+          <FormLabel htmlFor="minite" sx={labelStyle}>
+            分
+          </FormLabel>
+          <Input id="second" sx={inputStyle} type="second" />
+          <FormLabel htmlFor="second" sx={labelStyle}>
+            秒
+          </FormLabel>
+        </Box>
         <div>スタート</div>
         <div>一時停止</div>
       </FormControl>
@@ -22,6 +26,14 @@ export const Component: React.FC<Props> = ({ className }) => {
   );
 };
 
-export const Root: React.FC<Props> = (props) => {
+const labelStyle = {
+  padding: '8px',
+};
+
+const inputStyle = {
+  width: 200,
+};
+
+export const Root: React.FC<BoxProps> = (props) => {
   return <Component {...props} />;
 };
