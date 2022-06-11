efc0e5658a4070f4d500177688e468103b36401c
yuki.kakihara <argentina.kyg@gmail.com>
2022-06-11 16:02:41 +0900

    valueを管理できるようにした

--- components/templates/Root/Root.tsx
+++ components/templates/Root/Root.tsx
@@ -9,9 +9,28 @@ import {
   Input,
 } from '@chakra-ui/react';
 
-const Component: React.FC<BoxProps> = (props) => {
+type Values = {
+  minite: number;
+  second: number;
+};
+
+type ComponentProps = {
+  handleChange: (
+    e: React.ChangeEvent<HTMLInputElement>,
+    valueKey: keyof Values
+  ) => void;
+  values: Values;
+} & BoxProps;
+
+const Component: React.FC<ComponentProps> = ({
+  handleChange,
+  values,
+  ...boxProps
+}) => {
+  const { minite, second } = values;
+
   return (
-    <Box {...props}>
+    <Box {...boxProps}>
       <Heading as="h1" mb={16} size="xl" textAlign="center">
         StudyTimeRecorder
       </Heading>
@@ -19,13 +38,29 @@ const Component: React.FC<BoxProps> = (props) => {
         {/* <div>○分勉強したことを記録しました</div> */}
         <HStack justifyContent="center" spacing={8}>
           <HStack>
-            <Input id="minite" sx={inputStyle} type="minite" />
+            <Input
+              id="minite"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                handleChange(e, 'minite')
+              }
+              sx={inputStyle}
+              type="number"
+              value={minite}
+            />
             <FormLabel htmlFor="minite" sx={labelStyle}>
               分
             </FormLabel>
           </HStack>
           <HStack>
-            <Input id="second" sx={inputStyle} type="second" />
+            <Input
+              id="second"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                handleChange(e, 'second')
+              }
+              sx={inputStyle}
+              type="number"
+              value={second}
+            />
             <FormLabel htmlFor="second" sx={labelStyle}>
               秒
             </FormLabel>
@@ -46,6 +81,17 @@ const inputStyle = {
   width: 200,
 };
 
-export const Root: React.FC<BoxProps> = (props) => {
-  return <Component {...props} />;
+export const Root: React.FC<BoxProps> = (boxProps) => {
+  const initialValues: Values = { minite: 0, second: 0 };
+  const [values, setValues] = React.useState<Values>(initialValues);
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    valueKey: keyof Values
+  ): void => {
+    setValues({ ...values, [valueKey]: Number(e.target.value) });
+  };
+
+  return (
+    <Component handleChange={handleChange} values={values} {...boxProps} />
+  );
 };
